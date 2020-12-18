0c30034ca838dd973a2d771c29bb22fd2074cf78
sanjay Kumar <sanjay.mact4115@gmail.com>
2020-12-18 20:23:48 +0800

    min score of triangulation

--- src/dynamic/category8/NumberOfWaysOfTriangulation.java
+++ src/dynamic/category8/NumberOfWaysOfTriangulation.java
@@ -1,7 +1,7 @@
 package dynamic.category8;
 
 /**
- *1. You are given a number N, which represents the number of sides in a polygon.
+ * 1. You are given a number N, which represents the number of sides in a polygon.
  * 2. You have to find the total number of ways in which the given polygon can be triangulated.
  */
 public class NumberOfWaysOfTriangulation {
