abe9ccbd445ae7d92924132741bc13389f0bf84d
sanjay Kumar <sanjay.mact4115@gmail.com>
2020-12-18 11:04:04 +0800

    number-of-ways-of-triangulation.png

--- src/dynamic/category8/NumberOfWaysOfTriangulation.java
+++ src/dynamic/category8/NumberOfWaysOfTriangulation.java
@@ -1,7 +1,20 @@
 package dynamic.category8;
 
 /**
- *
+ *1. You are given a number N, which represents the number of sides in a polygon.
+ * 2. You have to find the total number of ways in which the given polygon can be triangulated.
  */
 public class NumberOfWaysOfTriangulation {
+
+    public static void main(String[] args) {
+        System.out.print(NumberOfWaysOfTriangulation.find(4));
+    }
+
+    /**
+     * Check number-of-ways-of-triangulation.png
+     */
+    public static int find(int n) {
+        if (n <2 ) return 0;
+        return CatalanNumber.find(n-2);
+    }
 }
