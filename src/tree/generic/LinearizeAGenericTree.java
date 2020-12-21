8a39913b6f1c54e62716d755d288252d83e639c2
sanjay Kumar <sanjay.mact4115@gmail.com>
2020-12-21 21:15:10 +0800

    lenearize a generic tree preorder way

---
+++ src/tree/generic/LinearizeAGenericTree.java
@@ -0,0 +1,65 @@
+package tree.generic;
+
+import tree.generic.structure.GenericTree;
+import tree.generic.structure.Node;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Stack;
+
+/**
+ * Preorder lenearize
+ */
+public class LinearizeAGenericTree {
+
+    public static void main(String[] args) {
+        var root = GenericTree.createTree(new int[] {10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1, -1});
+        GenericTree.display(root);
+        System.out.println();
+        LinearizeAGenericTree.recursion(root);
+        GenericTree.display(root);
+    }
+
+    /**
+     * During pre order save the last node and add current node as children to previous node
+     */
+    private static void lenearize(Node root) {
+        var stack = new Stack<Node>();
+        stack.push(root);
+        Node prev = null;
+        while (!stack.isEmpty()) {
+            var node = stack.pop();
+            if (prev != null){
+                prev.addChildren(node);
+            }
+            prev = node;
+            var children = node.getChildren();
+            for (int i=children.size()-1; i>=0; i--){
+                stack.push(children.get(i));
+                children.remove(i);
+            }
+        }
+    }
+
+    // O(n^2)
+    private static void recursion(Node root) {
+        for (var child : root.getChildren()){
+            recursion(child);
+        }
+        var children = root.getChildren();
+        while (children.size()>1){
+            var lastChild = children.remove(children.size() - 1);
+            var secondLastChild = children.get(children.size() - 1);
+            var secondLastChildTail = getTail(secondLastChild);
+            secondLastChildTail.addChildren(lastChild);
+        }
+    }
+
+    private static Node getTail(Node node) {
+        while (node.getChildren().size() == 1){
+            node = node.getChildren().get(0);
+        }
+        return node;
+    }
+}
