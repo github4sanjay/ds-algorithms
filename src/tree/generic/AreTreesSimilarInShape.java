a7dd375bc36317d6b37600fdcf78db4541ec6780
sanjay Kumar <sanjay.mact4115@gmail.com>
2020-12-22 18:20:18 +0800

    are trees similar in shape

---
+++ src/tree/generic/AreTreesSimilarInShape.java
@@ -0,0 +1,37 @@
+package tree.generic;
+
+import tree.generic.structure.GenericTree;
+import tree.generic.structure.Node;
+
+public class AreTreesSimilarInShape {
+
+    public static void main(String[] args) {
+        int[] arr1 = new int[]{10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1, -1};
+        var root1 = GenericTree.createTree(arr1);
+
+        int[] arr2 = new int[]{1, 2, 5, -1, 6, -1, -1, 3, 7, -1, 8, 11, -1, 12, -1, -1, 9, -1, -1, 4, 10, -1, -1, -1};
+        var root2 = GenericTree.createTree(arr2);
+
+        System.out.println(AreTreesSimilarInShape.check(root1, root2));
+
+
+        var root3 = GenericTree.createTree(new int[] {10, 20, -1, 30, 50, -1, 60, -1, -1, 40, -1, -1});
+        System.out.println(AreTreesSimilarInShape.check(root1, root3));
+    }
+
+    private static boolean check(Node root1, Node root2) {
+
+        var children1 = root1.getChildren();
+        var children2 = root2.getChildren();
+        if (children1.size() != children2.size()){
+            return false;
+        }
+
+        for (int i=0; i< children1.size(); i++){
+            if (!check(children1.get(i), children2.get(i))){
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/tree/generic/DistanceBetweenNodes.java
@@ -0,0 +1,30 @@
+package tree.generic;
+
+import tree.generic.structure.GenericTree;
+import tree.generic.structure.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DistanceBetweenNodes {
+
+    public static void main(String[] args) {
+        var root = GenericTree.createTree(new int[] {10 , 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1, -1});
+        System.out.println(DistanceBetweenNodes.recursion(root, 100, 110));
+
+    }
+
+    private static int recursion(Node root, int first, int second) {
+        var path1 = NodeToRootPathGenericTree.recursion(root, first);
+        var path2 = NodeToRootPathGenericTree.recursion(root, second);
+        int i = path1.size()-1;
+        int j = path2.size()-1;
+        while (i>=0 && j>=0 && path1.get(i).equals(path2.get(j))){
+            i--;
+            j--;
+        }
+        i++;
+        j++;
+        return i+j;
+    }
+}
