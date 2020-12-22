f6957033a7bb87e42a2d08baeb346ba577a8b496
sanjay Kumar <sanjay.mact4115@gmail.com>
2020-12-22 18:29:32 +0800

    are trees mirror in shape

--- src/tree/generic/AreTreesMirrorInShape.java
+++ src/tree/generic/AreTreesMirrorInShape.java
@@ -3,7 +3,7 @@ package tree.generic;
 import tree.generic.structure.GenericTree;
 import tree.generic.structure.Node;
 
-public class AreTreesSimilarInShape {
+public class AreTreesMirrorInShape {
 
     public static void main(String[] args) {
         int[] arr1 = new int[]{10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1, -1};
@@ -12,16 +12,19 @@ public class AreTreesSimilarInShape {
         int[] arr2 = new int[]{1, 2, 5, -1, 6, -1, -1, 3, 7, -1, 8, 11, -1, 12, -1, -1, 9, -1, -1, 4, 10, -1, -1, -1};
         var root2 = GenericTree.createTree(arr2);
 
-        System.out.println(AreTreesSimilarInShape.check(root1, root2));
+        System.out.println(AreTreesMirrorInShape.check(root1, root2));
 
 
         var root3 = GenericTree.createTree(new int[] {10, 20, -1, 30, 50, -1, 60, -1, -1, 40, -1, -1});
-        System.out.println(AreTreesSimilarInShape.check(root1, root3));
+        System.out.println(AreTreesMirrorInShape.check(root1, root3));
+
+        var root4 = GenericTree.createTree(new int[] {100, 200, -1, 300, 500, -1, 600, -1, -1, 400, -1, -1});
+        System.out.println(AreTreesMirrorInShape.check(root4, root3));
     }
 
     /**
-     * root1 and root2 should have same number of children and all children at same position should
-     * have same number of children
+     * Number of children of root1 first child and number of children of root2 last child should be equal
+     * and same for other children at different positions
      */
     private static boolean check(Node root1, Node root2) {
 
@@ -32,7 +35,7 @@ public class AreTreesSimilarInShape {
         }
 
         for (int i=0; i< children1.size(); i++){
-            if (!check(children1.get(i), children2.get(i))){
+            if (!check(children1.get(i), children2.get(children1.size()-i-1))){
                 return false;
             }
         }
