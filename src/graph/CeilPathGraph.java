f62d519176b69a84e473429a406307a9a595befa
sanjay Kumar <sanjay.mact4115@gmail.com>
2020-12-26 16:49:29 +0800

    find smalles, longest, floor, ceil path

---
+++ src/graph/CeilPathGraph.java
@@ -0,0 +1,77 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CeilPathGraph {
+
+    public static void main(String[] args) {
+        var graph = new Graph(new int[][]{
+                {0, 1, 10},
+                {1, 2, 10},
+                {2, 3, 10},
+                {0, 3, 40},
+                {3, 4, 2 },
+                {4, 5, 3 },
+                {5, 6, 3 },
+                {4, 6, 8 },
+                {2, 5, 5 }
+        }, 7);
+
+        System.out.println(CeilPathGraph.findPath(graph, 0, 6, 30)); // Answer{path=[0, 1, 2, 5, 4, 6], weight=36}
+    }
+
+    /**
+     * if source and destination are equal then check
+     *  1. if current weight is greater than ceil
+     *  2. and smaller than previous path weight
+     * other wise check in all neighbour
+     * with the source = neighbour and same destination
+     */
+    public static Answer findPath(Graph graph, int vertex1, int vertex2, int ceil) {
+        var visited = new boolean[graph.getEdges().length];
+        var currentPath = new ArrayList<Integer>();
+        var answer = new Answer();
+        answer.weight = Integer.MAX_VALUE;
+        currentPath.add(vertex1);
+        findPath(graph, vertex1, vertex2, ceil, visited, currentPath, 0, answer);
+        return answer;
+    }
+
+    private static void findPath(Graph graph, int src, int dest, int ceil, boolean[] visited, List<Integer> currentPath, int currentWeight, Answer answer) {
+
+        if (src == dest){
+            if (currentWeight < answer.weight && currentWeight > ceil){
+                answer.path = new ArrayList<>(currentPath);
+                answer.weight = currentWeight;
+            }
+            return;
+        }
+
+        visited[src] = true;
+
+        var edges = graph.getEdges();
+        var vertex1Edges = edges[src];
+        for (var edge : vertex1Edges){
+            if (!visited[edge.getNbr()]){
+                currentPath.add(edge.getNbr());
+                findPath(graph, edge.getNbr(), dest, ceil, visited, currentPath, currentWeight+edge.getWt(), answer);
+                currentPath.remove(currentPath.size()-1);
+            }
+        }
+        visited[src] = false;
+    }
+
+    public static class Answer {
+        List<Integer> path;
+        int weight;
+
+        @Override
+        public String toString() {
+            return "Answer{" +
+                    "path=" + path +
+                    ", weight=" + weight +
+                    '}';
+        }
+    }
+}
---
+++ src/graph/FindAllPathGraph.java
@@ -0,0 +1,57 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindAllPathGraph {
+
+    public static void main(String[] args) {
+        var graph = new Graph(new int[][]{
+                {0, 1, 10},
+                {1, 2, 10},
+                {2, 3, 10},
+                {0, 3, 10},
+                {3, 4, 10},
+                {4, 5, 10},
+                {5, 6, 10},
+                {4, 6, 10}
+        }, 7);
+
+        System.out.println(FindAllPathGraph.findPath(graph, 0, 6));
+    }
+
+    /**
+     * if source and destination are equal then add current path to final list
+     * other wise check in all neighbour
+     * with the source = neighbour and same destination
+     */
+    public static List<List<Integer>> findPath(Graph graph, int vertex1, int vertex2) {
+        var visited = new boolean[graph.getEdges().length];
+        var currentPath = new ArrayList<Integer>();
+        var allPaths = new ArrayList<List<Integer>>();
+        currentPath.add(vertex1);
+        findPath(graph, vertex1, vertex2, visited, currentPath, allPaths);
+        return allPaths;
+    }
+
+    private static void findPath(Graph graph, int src, int dest, boolean[] visited, List<Integer> currentPath, List<List<Integer>> paths) {
+
+        if (src == dest){
+            paths.add(new ArrayList<>(currentPath));
+            return;
+        }
+
+        visited[src] = true;
+
+        var edges = graph.getEdges();
+        var vertex1Edges = edges[src];
+        for (var edge : vertex1Edges){
+            if (!visited[edge.getNbr()]){
+                currentPath.add(edge.getNbr());
+                findPath(graph, edge.getNbr(), dest, visited, currentPath, paths);
+                currentPath.remove(currentPath.size()-1);
+            }
+        }
+        visited[src] = false;
+    }
+}
