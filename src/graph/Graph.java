03650f8e9d5d414158f283d15e41a3c73f101833
sanjay Kumar <sanjay.mact4115@gmail.com>
2020-12-27 19:31:08 +0800

    topological sort

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -22,6 +22,23 @@ public class Graph {
         }
     }
 
+    public Graph(int[][] edges, int vertices, boolean isDirected) {
+        this.edges = new ArrayList[vertices];
+        for (int i=0;i< vertices;i++){
+            this.edges[i] = new ArrayList<>();
+        }
+
+        for (int[] edge : edges) {
+            var edge1 = this.edges[edge[0]];
+            edge1.add(new Edge(edge[0], edge[1], edge.length >= 3 ? edge[2] : 0)); //  put 0 when weight not passed
+
+            if (!isDirected){
+                var edge2 = this.edges[edge[1]];
+                edge2.add(new Edge(edge[1], edge[0], edge.length >= 3 ? edge[2]: 0)); //  put 0 when weight not passed
+            }
+        }
+    }
+
     public List<Edge>[] getEdges() {
         return edges;
     }
