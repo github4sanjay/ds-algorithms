4619d9499adb80e000d86d01c4c5d21b2751b912
sanjay4finxflo <89559819+sanjay4finxflo@users.noreply.github.com>
2022-05-08 16:37:43 +0800

    house robbery in binary tree

---
+++ src/main/java/com/github4sanjay/dsalgo/tree/binary/HouseRobberInBinaryTree.java
@@ -0,0 +1,69 @@
+package com.github4sanjay.dsalgo.tree.binary;
+
+import com.github4sanjay.dsalgo.tree.binary.structure.BinaryNode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 1. You are given a partially written function to solve. 2. The thief has found himself a new
+ * place for his thievery again. There is only one entrance to this area, called the "root." Besides
+ * the root, each house has one and only one parent house. After a tour, the smart thief realized
+ * that "all houses in this place forms a binary tree". It will automatically contact the police if
+ * two directly-linked houses were broken into on the same night. Determine the maximum amount of
+ * money the thief can rob tonight without alerting the police. 3. Input and Output is managed for
+ * you.
+ *
+ * <p>Constraints 0 <= Number of Nodes <= 10^9 -10^9 <= value of Node data <= 10^9
+ *
+ * <p>Format Input Input is managed for you.
+ *
+ * <p>Output Output is managed for you.
+ *
+ * <p>Example Sample Input
+ *
+ * <p>15 1 1 -1 1 1 -1 1 -1 -1 1 -1 -1 1 -1 -1
+ *
+ * <p>Sample Output 4
+ */
+public class HouseRobberInBinaryTree {
+
+  public static Integer find(BinaryNode node) {
+    var answer = new Answer();
+    find(node, answer);
+    return answer.getMoney();
+  }
+
+  private static Money find(BinaryNode node, Answer answer) {
+    if (node == null) return new Money();
+    var lChild = find(node.getLeft(), answer);
+    var rChild = find(node.getRight(), answer);
+    // max of with or without robbery
+    answer.setMoney(
+        Math.max(
+            lChild.getWithoutRobbery() + rChild.getWithoutRobbery() + node.getData(),
+            lChild.withRobbery + rChild.withRobbery));
+    return new Money(
+        lChild.getWithoutRobbery() + rChild.getWithoutRobbery() + node.getData(),
+        lChild.withRobbery + rChild.withRobbery);
+  }
+
+  @Getter
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class Money {
+    private int withRobbery;
+    private int withoutRobbery;
+  }
+
+  @Setter
+  @Getter
+  public static class Answer {
+    private int money;
+
+    public void add(int more) {
+      money = money + more;
+    }
+  }
+}
---
+++ src/test/java/com/github4sanjay/dsalgo/tree/binary/HouseRobberInBinaryTreeTest.java
@@ -0,0 +1,22 @@
+package com.github4sanjay.dsalgo.tree.binary;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.github4sanjay.dsalgo.tree.binary.structure.BinaryTreeUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class HouseRobberInBinaryTreeTest {
+  @Test
+  public void test1() {
+    var root1 =
+        BinaryTreeUtil.create(new int[] {1, 1, -1, 1, 1, -1, 1, -1, -1, 1, -1, -1, 1, -1, -1});
+    Assertions.assertEquals(4, HouseRobberInBinaryTree.find(root1));
+
+    var root2 = BinaryTreeUtil.create(new int[] {3, 2, -1, 3, -1, -1, 3, -1, 1, -1, -1});
+    Assertions.assertEquals(7, HouseRobberInBinaryTree.find(root2));
+
+    var root3 = BinaryTreeUtil.create(new int[] {3, 4, 1, -1, -1, 3, -1, -1, 5, -1, 1, -1, -1});
+    Assertions.assertEquals(9, HouseRobberInBinaryTree.find(root3));
+  }
+}
